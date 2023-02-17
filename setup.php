1064473995c756b80e1d9e60dc8e55e5bc8ced92
Colin Walker <colin@colinwalker.blog>
2023-02-17 22:34:06 +0000

    Minor updates

--- setup.php
+++ setup.php
@@ -71,7 +71,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
     <title>Setup</title>
-    <link rel="stylesheet" href="/style_min.css">
+    <link rel="stylesheet" href="style.css">
     <style type="text/css">
         .wrapper{ width: 450px; }
     </style>
